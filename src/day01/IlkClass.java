6b7e12a5a394f5f7431c2d6ed05878ef06efbefa
aysekilic34 <epsilonuzaklik@gmail.com>
2020-11-23 11:30:08 +0300

    düzeltildi

--- src/day01/IlkClass.java
+++ src/day01/IlkClass.java
@@ -4,13 +4,9 @@ public class IlkClass {
 
 	public static void main(String[] args) {
 		System.out.println("ayseden selamlar");
-<<<<<<< HEAD
-		System.out.println("cakýsma bulmaya calýstýk");
-=======
-		System.out.println("fatma selamlar");
+        System.out.println("fatma selamlar");
 		System.out.println("ali selamlar");
-		System.out.println("nÄ±hal selamlar");
->>>>>>> refs/remotes/origin/main
+		System.out.println("nihal selamlar");
 	}
 
 }
