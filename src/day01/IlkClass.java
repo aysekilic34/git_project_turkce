ea4c7c570d77cb2dab6f8bbffe1589e03c019dd3
aysekilic34 <74234116+aysekilic34@users.noreply.github.com>
2020-11-23 11:16:56 +0300

    Update IlkClass.java

--- src/day01/IlkClass.java
+++ src/day01/IlkClass.java
@@ -4,6 +4,9 @@ public class IlkClass {
 
 	public static void main(String[] args) {
 		System.out.println("ayseden selamlar");
+		System.out.println("fatma selamlar");
+		System.out.println("ali selamlar");
+		System.out.println("nıhal selamlar");
 	}
 
 }
